e8c7d37dc82e0de2d673218e3736da2478e06641
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2021-04-07 06:54:25 -0600

    Add Event Listener (still needs equip and unequip)

---
+++ src/daylightnebula/mmorpgplugin/EventListener.java
@@ -0,0 +1,34 @@
+package daylightnebula.mmorpgplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class EventListener implements Listener {
+
+    public EventListener(Plugin plugin) {
+        Bukkit.getScheduler().runTaskTimer(plugin, new BukkitRunnable() {
+            @Override
+            public void run() {
+                Update();
+            }
+        }, 0L, 1L);
+    }
+
+    @EventHandler
+    public void EntityDamageByEntity(EntityDamageByEntityEvent event) {
+
+    }
+
+    @EventHandler
+    public void ProjectileLaunch(ProjectileLaunchEvent event) {
+    }
+
+    public void Update() {
+
+    }
+}
